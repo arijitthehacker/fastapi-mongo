1743e84f6e48964095f466499ec86fda743a7a77
arijitthehacker <senarijit1618@gmail.com>
2024-04-22 07:54:48 +0530

    feat: get, update, delete methods addded

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from typing import List
 from bson import ObjectId
 from fastapi import FastAPI, HTTPException
 from fastapi.params import Depends
@@ -5,13 +6,14 @@ from pymongo import MongoClient
 from pydantic import BaseModel
 import logging
 from contextlib import asynccontextmanager
+import os
 
 from models import CreateItem, ReadItem, UpdateItem, DeleteItem
 
 logger = logging.getLogger("uvicorn.error")
 
 async def connect_to_mongo():
-    client = MongoClient("mongodb://localhost:27017")
+    client = MongoClient(os.getenv("MONGO_URL", "mongodb://localhost:27017"))
     db = client["fastapi"]
     logger.debug("Connected to MongoDB:")
     return db
@@ -40,3 +42,32 @@ async def read_item(id: str):
     if not item:
         raise HTTPException(status_code=404, detail=f"Item {id} not found")
     return item
+
+@app.get("/items", response_model=List[ReadItem])
+async def read_all():
+    db = await connect_to_mongo()
+    items = []
+    for item in db["items"].find():
+        item['id'] = str(item['_id'])
+        items.append(item)
+    return items
+
+@app.put("/items/{id}", response_model=UpdateItem)
+async def update_item(id: str, item: UpdateItem):
+    db = await connect_to_mongo()
+    updated_item = db["items"].find_one({"_id": ObjectId(id)})
+    if not updated_item:
+        raise HTTPException(status_code=404, detail=f"Item {id} not found")
+    update_data = item.dict(exclude_unset=True)
+    for key, value in update_data.items():
+        if value is not None:
+            updated_item[key] = value
+    db["items"].update_one({"_id": ObjectId(id)}, {"$set": updated_item})
+    return updated_item
+    
+@app.delete("/items/{id}")
+async def delete_item(id: str):
+    db = await connect_to_mongo()
+    deleted_count = db["items"].delete_one({"_id": ObjectId(id)}).deleted_count
+    if not deleted_count:
+        raise HTTPException(status_code=404, detail=f"Item {id} not found")
